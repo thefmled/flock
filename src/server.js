5337e0f8fb9e75b24bf09a777e14d4ccd50da287
Rushali Chakraborty <rushali.c1710@gmail.com>
2026-05-23 19:13:53 +0530

    Uncomment venues route in server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.get('/health', (req, res) => {
 
 // Routes (we'll add these as we build)
 app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/venues', require('./routes/venues'));
+app.use('/api/venues', require('./routes/venues'));
 // app.use('/api/queue', require('./routes/queue'));
 
 app.listen(PORT, () => {
